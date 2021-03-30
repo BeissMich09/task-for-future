377e5f8e23b0338c7b4070acfe3da78c4eb50a78
Rothaarige Fuchs <54771778+BeissMich09@users.noreply.github.com>
2021-03-30 20:53:10 +0300

    Update UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -12,7 +12,7 @@ import {
 class UsersContainer extends React.Component {
   dataRequest = (value) => {
     fetch(
-      `http://www.filltext.com/?rows=${value}&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
+      `https://cors-anywhere.herokuapp.com///www.filltext.com/?rows=${value}&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`
     )
       .then((res) => res.json())
       .then((data) => {
